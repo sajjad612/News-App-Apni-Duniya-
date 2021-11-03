9c41e178328fb7ce4b46cb0a32a3b61c009d6caf
sajjad <sajjadalam2000@gmail.com>
2021-11-03 18:47:09 +0530

    Splash commit

---
+++ app/src/main/java/com/example/news/Splash.java
@@ -0,0 +1,29 @@
+package com.example.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(2500);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intend  = new Intent(Splash.this,ChooseCategory.class);
+                    startActivity(intend);
+                }
+            }
+        };thread.start();
+
+
+    }
+}
